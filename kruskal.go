d83da24b68237d0accb68b6acc70872b78fdb418
hauke96 <mail@hauke-stieler.de>
2015-12-11 13:22:33 +0100

    Implemented a sorting algorithm (bubble-sort) to sort all keys of the map of edges. This is used to iterate correctly over the edges to get the minimum spanning tree (s. Kruskal algorithm).

--- kruskal.go
+++ kruskal.go
@@ -25,6 +25,55 @@ func main() {
 
 	//TODO better output + output of minimal spanning tree
 
-	fmt.Println(g.vertexMap)
-	fmt.Println(g.edgeMap)
+	//Output of the edge map
+	fmt.Println("\n\nEdge map:\n")
+	for _, e := range g.edgeMap {
+		fmt.Println(e)
+	}
+
+	// Start Kruskal algorithm
+	fmt.Println("\n\nStart kruskal:\n")
+	A := make([]edge, 0)
+	// iterates over the list of sorted keys (e)
+	for e, i := getBounds(g.edgeMap), 0; i < len(e); i++ {
+		fmt.Println(i, " - ", e[i])
+		for _, e := range g.edgeMap[e[i]] {
+			fmt.Println("    ", e)
+			if findSet(&e.x) != findSet(&e.y) {
+				A = append(A, e)
+				union(&e.x, &e.y)
+			}
+		}
+	}
+	fmt.Println("\n\nTree:\n")
+
+	//Output of the tree
+	for _, e := range A {
+		fmt.Println(e)
+	}
+}
+
+// Gets a sorted list of all keys of the given map.
+func getBounds(edges map[int][]edge) []int {
+	output := make([]int, 0)
+
+	// get all keys
+	for i, _ := range edges {
+		output = append(output, i)
+	}
+
+	// sort list of keys
+	// this uses bubble sort because it's easy
+	fmt.Println("pre: ", output)
+	for n := len(output); n > 1; n-- {
+		for i := 0; i < n-1; i++ {
+			if output[i] > output[i+1] {
+				temp := output[i]
+				output[i] = output[i+1]
+				output[i+1] = temp
+			}
+		}
+	}
+	fmt.Println("after: ", output)
+	return output
 }
